547917b083a074bd06820890002bca260399b12a
Tracy Ngo <tngo1206@gmail.com>
2019-05-28 20:29:35 -0700

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,11 @@
+class Puppy
+
+
+attr_accessor :months_old, :name, :breed
+
+  def initialize(name, breed, months_old)
+    @name = name
+    @breed = breed
+    @months_old = months_old
+  end
+end
